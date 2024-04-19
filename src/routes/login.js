22299e12a2aa687b839f02b7300c259f9d3ae225
Kaisei Yoneyama <97094702+mikrogeophagus@users.noreply.github.com>
2024-04-19 22:02:01 +0900

    ログイン表示の削除と、ボタンのスタイルの追加

--- src/routes/login.js
+++ src/routes/login.js
@@ -5,17 +5,14 @@ const layout = require("../layout");
 const app = new Hono();
 
 app.get("/", (c) => {
-  const { user } = c.get("session") ?? {};
   return c.html(
     layout(
       c,
       "Login",
       html`
-        <h1>Login</h1>
-        <a href="/auth/github">GitHub でログイン</a>
-        ${user
-          ? html`<p>現在 ${user.login} でログイン中</p>`
-          : ""}
+        <a href="/auth/github" class="btn btn-primary my-3">
+          GitHub でログイン
+        </a>
       `,
     ),
   );
